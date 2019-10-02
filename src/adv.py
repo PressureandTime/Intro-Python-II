ad4ba36dd4454f1b1c149bf2874437292c5f4cd5
Petar Scepanovic <hiborija@gmail.com>
2019-10-02 17:13:35 +0200

    wrote all the classes and the player can go to rooms, still have not implemented all the MVP requirements

--- src/adv.py
+++ src/adv.py
@@ -1,41 +1,65 @@
 from room import Room
+from player import Player
+import textwrap
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    "outside": Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    "foyer": Room(
+        "Foyer",
+        """Dim light filters in from the south. Dusty
+passages run north and east.""",
+    ),
+    "overlook": Room(
+        "Grand Overlook",
+        """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+the distance, but there is no way across the chasm.""",
+    ),
+    "narrow": Room(
+        "Narrow Passage",
+        """The narrow passage bends here from west
+to north. The smell of gold permeates the air.""",
+    ),
+    "treasure": Room(
+        "Treasure Chamber",
+        """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",
+    ),
 }
-
 
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room["outside"].n_to = room["foyer"]
+room["foyer"].s_to = room["outside"]
+room["foyer"].n_to = room["overlook"]
+room["foyer"].e_to = room["narrow"]
+room["overlook"].s_to = room["foyer"]
+room["narrow"].w_to = room["foyer"]
+room["narrow"].n_to = room["treasure"]
+room["treasure"].s_to = room["narrow"]
 
-#
 # Main
-#
+
+hero = Player("Conan", "outside")
+while True:
+    direction = input("Where do you want to go? (n,s,e,w)  ")
+    print(hero.current_room)
+    if direction == "n":
+        if room[hero.current_room].n_to is not None:
+            hero.current_room = room[hero.current_room].name
+    print(hero.current_room)
+    # for place in room:
+    #     foyer = Room("foyer", "Dim light filters in from the south,Dusty passages run north and east.")
+    #     place = foyer
+    #     print(place)
+
+    # wrapper = textwrap.TextWrapper(width=50)
+    # word_list = wrapper.wrap(text=direction)
+    # for element in word_list:
+    #     print(element)
 
 # Make a new player object that is currently in the 'outside' room.
 
