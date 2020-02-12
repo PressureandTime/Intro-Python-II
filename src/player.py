902c483bcfd2c4a8643b95da6f3d5d4cd6be3b12
Petar Scepanovic <hiborija@gmail.com>
2020-02-12 18:20:25 +0100

    wrote Player class, and methods for adding and removing items from inventory, both methods working

--- src/player.py
+++ src/player.py
@@ -1,2 +1,31 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self, room):
+        self.room = room
+        self.inventory = []
+
+    def way_to_go(self, d):
+        print(d)
+
+    def add_the_item(self, item):
+        self.inventory.append(item)
+
+    def remove_the_item(self, item):
+        for thing in self.inventory:
+            if item == thing:
+                self.inventory.remove(item)
+        print('item was not found')
+
+
+conan = Player('field')
+
+conan.add_the_item('sword')
+conan.add_the_item('shield')
+conan.add_the_item('axe')
+
+print(vars(conan))
+
+conan.remove_the_item('shield')
+print(vars(conan))
